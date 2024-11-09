c6fd8a78ea2105fb2c487e5b7d1006baa79f9530
Lakshminarayan-p <lakanna6@gmail.com>
2024-11-09 10:48:53 +0530

    home screen

--- app/src/main/java/com/example/masterquiz/MainActivity.java
+++ app/src/main/java/com/example/masterquiz/MainActivity.java
@@ -1,24 +1,67 @@
 package com.example.masterquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        CardView natureCard = findViewById(R.id.card_nature);
+        CardView scienceCard = findViewById(R.id.card_science);
+        CardView csCard = findViewById(R.id.card_computer_science);
+
+        natureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizScreen("Nature");
+            }
         });
+
+        scienceCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizScreen("Science");
+            }
+        });
+
+        csCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizScreen("Computer Science");
+            }
+        });
+    }
+
+    private void openQuizScreen(String topic) {
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        intent.putExtra("TOPIC", topic);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_history) {
+            Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
